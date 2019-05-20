435e5ac022f95e7d09632e528396243dfa4ca4b7
tuanpham1406 <tuanpham1406@gmail.com>
2019-05-20 11:37:37 +0700

    [Thực hành] Tạo trang web hiển thị thời gian hệ thống

---
+++ src/SeverTimeServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SeverTimeServlet", urlPatterns = "/index")
+public class SeverTimeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello world</h1>");
+        writer.println("</html>");
+    }
+}
